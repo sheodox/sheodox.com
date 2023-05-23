123aa937978c37d437c0e329c1f382f903b86c41
sheodox <imagamer27@msn.com>
2023-05-23 18:53:08 -0500

    Added Dice Chest

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,13 +2,15 @@ import ContextReviews from './projects/context-reviews';
 import Konshuu from './projects/konshuu';
 import JimakuPlayer from './projects/jimaku-player';
 import WellRead from './projects/well-read';
+import DiceChest from './projects/dicechest';
 
 export default function Projects() {
 	return (
 		<section className="flex flex-col gap-14 mb-14">
 			<ContextReviews />
-			<JimakuPlayer />
 			<Konshuu />
+			<JimakuPlayer />
+			<DiceChest />
 			<WellRead />
 		</section>
 	);
