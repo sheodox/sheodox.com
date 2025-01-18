32edf5a7f239be565faeb50334cbc4e2b1d273d9
sheodox <imagamer27@msn.com>
2025-01-17 23:36:42 -0600

    Fix 'rel'

--- components/external-link.tsx
+++ components/external-link.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export default function ExternalLink(props: PropsWithChildren<{ href: string; className?: string; rel?: string }>) {
 	return (
-		<a href={props.href} className={props.className} rel="noreferrer {rel ?? ''}" target="_blank">
+		<a href={props.href} className={props.className} rel={`noreferrer ${props.rel ?? ''}`} target="_blank">
 			{props.children}
 		</a>
 	);
